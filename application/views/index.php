65bd998fb27cf93ca87e2baba8887be3b085d53f
Todd Enders <tenders64@gmail.com>
2016-05-07 09:49:49 -0700

    favicon linked in head

--- application/views/index.php
+++ application/views/index.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
 	<title>H2O2 - Capitol Hill</title>
 	<link rel="stylesheet" href="/assets/css/materialize.min.css">
 	<link rel="stylesheet" href="/assets/css/stylesheet2.css">
